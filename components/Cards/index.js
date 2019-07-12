97ea7c775f56f10a09f0fcb5ddc8b73fab34b59a
tjvonbr <51134664+tjvonbr@users.noreply.github.com>
2019-07-12 12:15:14 -0500

    Finished MVP requirements

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,55 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardContainer = document.querySelector('.cards-container');
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        articleData = response.data.articles;
+        console.log(response.data.articles);
+        
+        const articleKeys = Object.keys(articleData);
+        console.log(articleKeys);
+
+        articleKeys.forEach(art => {
+            articleData[art].forEach(indyData => {
+                const cardCreator = articleCreator(indyData);
+                cardContainer.appendChild(cardCreator);
+            })
+        })
+    })
+    .catch(error => {
+        console.log('Sorry, but the Lambda Times server is currently unavailable.', error);
+    })
+
+function articleCreator(receivedData) {
+
+    // Create elements
+    const card = document.createElement('div');
+    const cardHeadline = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardImgContainer = document.createElement('div');
+    const cardImg = document.createElement('img');
+    const cardSignature = document.createElement('span');
+
+    // Class lists
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthor.classList.add('author');
+    cardImgContainer.classList.add('img-container');
+
+    // Text content
+    cardHeadline.textContent = receivedData.headline;
+    cardImg.src = receivedData.authorPhoto;
+    cardSignature.textContent = receivedData.authorName;
+
+    // Append children
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthor);
+    cardAuthor.appendChild(cardImgContainer);
+    cardAuthor.appendChild(cardSignature);
+    cardImgContainer.appendChild(cardImg);
+
+    return card;
+}
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,34 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicContainer = document.querySelector('.topics');
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        // console.log(response.data.topics);
+        const passedData = response.data.topics;
+
+        passedData.forEach(item => {
+            const newTab = tabCreator(item);
+            topicContainer.appendChild(newTab);
+        })
+    })
+    .catch(error => {
+        console.log('Sorry, but the Lambda Times server is currently unavailable.', error);
+    })
+
+
+function tabCreator(receivedData) {
+    
+    // Create element
+    const tabTopic = document.createElement('div');
+
+    // Class list
+    tabTopic.classList.add('tab');
+
+    // Text Content
+    tabTopic.textContent = `${receivedData}`;
+
+    return tabTopic;
+}
